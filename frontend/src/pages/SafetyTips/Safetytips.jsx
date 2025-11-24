e791792d7de8a3f3d3b1d08a363e390a1e70fd2e
abbywilson11 <awils141@uottawa.ca>
2025-11-24 14:59:06 -0500

    added text to safety page

--- frontend/src/pages/SafetyTips/Safetytips.jsx
+++ frontend/src/pages/SafetyTips/Safetytips.jsx
@@ -9,9 +9,21 @@ export default function SafetyTips() {
 
       <div className="safety-section">
         <h2 className="section-title">Tips</h2>
-        <p className="section-text">
-          This is an example section. You can place any information here.
-        </p>
+        <h3 className="section-subtitle">Stay Alert</h3>
+        <ul className="section-text">
+          <li>Keep your head up</li>
+          <li>Make eye contact with people passing by</li>
+          <li>Don’t wear headphones</li>
+          <li>Don’t wear a hoodie – it limits your peripheral vision</li>
+        </ul>
+
+        <h3 className="section-subtitle">Plan Your Route</h3>
+          <ul className="section-text">
+            <li>Check what your route is going to be ahead of time</li>
+            <li>Let a friend or family member know your estimated time of return</li>
+            <li>Wear reflective clothing to be more easily spotted by cars and bikes</li>
+          </ul>
+
       </div>
 
       <div className="safety-section">
